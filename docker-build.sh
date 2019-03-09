ee7439f8d0f759d2048cb8097f339c1404023852
Sualeh Fatehi <sualeh@hotmail.com>
2019-03-09 08:09:05 -0500

    Changing version to 15.05.01

--- docker-build.sh
+++ docker-build.sh
@@ -4,14 +4,14 @@ set -e
 # ## DOCKER
 echo "Creating SchemaCrawler Docker container"
 
-SCHEMACRAWLER_VERSION=15.04.01
+SCHEMACRAWLER_VERSION=15.05.01
 
 # Create SchemaCrawler distribution
 ./schemacrawler-distribution.sh
 
 # Print Docker version
 pwd
-docker version  
+docker version
 
 # Build Docker image
 docker build -t schemacrawler/schemacrawler .
@@ -21,7 +21,7 @@ docker tag schemacrawler/schemacrawler schemacrawler/schemacrawler:latest
 # Deploy image to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push schemacrawler/schemacrawler
-docker logout  
+docker logout
 
 # Remove local image
 # docker rm schemacrawler/schemacrawler
